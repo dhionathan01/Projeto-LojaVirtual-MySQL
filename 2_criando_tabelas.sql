3f3c0512f5e21481d9fccaaa879580bbb3529665
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-07-17 14:32:01 -0300

    Definindo Foreign key

--- 2_criando_tabelas.sql
+++ 2_criando_tabelas.sql
@@ -4,10 +4,15 @@
  AUTO_INCREMENT, defini um valor N e toda vez que for adicionado algo nessa coluna será adicionado como N+1 automaticamente pelo próprio SGBD.
  */
 
-CREATE TABLE produtos(
+CREATE TABLE tb_produtos(
     id_produto INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome_produto VARCHAR(200) NOT NULL,
     valor_produto FLOAT(8,2) NOT NULL
 );
 
-CREATE Table tb_descricoes_tec
+CREATE TABLE tb_descricoes_tenicas(
+	id_descricao INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fk_id_produto INT NOT NULL,
+    descricao_tecnica TEXT NOT NULL,
+    FOREIGN KEY(fk_id_produto) REFERENCES tb_produtos(id_produto) /* atribuindo uma FOREIGN KEY a tabela */
+);
