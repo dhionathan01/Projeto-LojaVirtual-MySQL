b3d57a8ac707ff0bafa427b56181a2a62afe7938
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-07-17 14:51:52 -0300

    Renomeando Arquivo

---
+++ 2_criando_tabelas_um_para_um.sql
@@ -0,0 +1,18 @@
+/*
+ O termo 'PRIMARY KEY', define essa coluna como chave primária, ou seja não vai se repetir. 
+
+ AUTO_INCREMENT, defini um valor N e toda vez que for adicionado algo nessa coluna será adicionado como N+1 automaticamente pelo próprio SGBD.
+ */
+
+CREATE TABLE tb_produtos(
+    id_produto INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_produto VARCHAR(200) NOT NULL,
+    valor_produto FLOAT(8,2) NOT NULL
+);
+
+CREATE TABLE tb_descricoes_tecnicas(
+	id_descricao INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fk_id_produto INT NOT NULL,
+    descricao_tecnica TEXT NOT NULL,
+    FOREIGN KEY(fk_id_produto) REFERENCES tb_produtos(id_produto) /* atribuindo uma FOREIGN KEY a tabela */
+);
